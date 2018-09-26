c7a31bbb0f05625f92c2fffcf8752f6d0f51679d
Sanjeev Ghimire <gsanjeev7@gmail.com>
2018-09-26 14:49:42 -0500

    Use requests to fetch images
    * testing classify
    
    * json read fix + cleanups
    
    * permission sc fix
    
    * Update bot_moderator_function.py

--- functions/bot_moderator_function.py
+++ functions/bot_moderator_function.py
@@ -1,13 +1,33 @@
-import sys
-import os
-import urllib
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
-from watson_developer_cloud import VisualRecognitionV3
+import os
+import requests
+import shutil
+import subprocess
+import sys
+import urllib
+
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 import Features, EntitiesOptions, KeywordsOptions
 
+
 SUPPORTED_IMAGES = ['image/jpeg', 'image/jpg', 'image/png']
 
+
+# main() will be run when you invoke this action
+# @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+# @return The output of this action, which must be a JSON object.
 def main(event):
     print('Validating message...')
     print(event)
@@ -29,28 +49,35 @@ def main(event):
         file_id = file_details['id']
         
         print('Downloading image...')
-        image_bytes = download_image(event,image_url)
+        image_bytes = download_image_requests(event,image_url)
         print('Saving image locally ...')
-        with open('./file.jpg', 'wb') as jpgFile:
-           jpgFile.write(image_bytes)
-            
-        print('Checking image for explicit content...' + image_url)        
-        visual_recognition = VisualRecognitionV3(
-            '2018-03-19',            
-            api_key=event['VISUAL_RECOGNITION_IAM_APIKEY'])
+        # with open('./file.jpg', 'wb') as jpgFile:
+        #   jpgFile.write(image_bytes)
         
-        with open('./file.jpg', 'rb') as images_file:
-            classes = visual_recognition.classify(
-                images_file,
-                threshold='0.6',
-                classifier_ids='default,explicit')
+        with open('./file.jpg', 'wb') as jpgFile:
+            image_bytes.raw.decode_content = True    
+            shutil.copyfileobj(image_bytes.raw, jpgFile)
+            
+        subprocess.call(["ls", "-l", "file.jpg"])
+        
+        print('Checking image for explicit content...' + image_url)  
+            
+        with open('./file.jpg', 'rb') as image_file:
+            classes = requests.post(
+                'https://gateway.watsonplatform.net/visual-recognition/api/v3/classify',
+                auth=('apikey', event['VISUAL_RECOGNITION_IAM_APIKEY']),
+                files={'images_file': image_file},
+                params={
+                    'version': '2016-05-20',
+                    'classifier_ids': 'explicit'})
             
         print('image classified for explicit content')
-                
-        print(json.dumps(classes, indent=2))
+        response_classes = classes.json();        
+        print(response_classes)
         is_explicit = False
+        print(response_classes['images'][0]['classifiers'])
             
-        for i in classes['images'][0]['classifiers']:
+        for i in response_classes['images'][0]['classifiers']:
             if i['classifier_id'] == 'explicit':
                 print(i['classes'][0]['class'])
                 if i['classes'][0]['class'] == 'explicit':
@@ -60,7 +87,7 @@ def main(event):
             print('Image displays explicit content- deleting from Slack Shared Files...')
             delete_image(event, file_id)
             print('Posting message to channel to notify users of file deletion...')
-            post_message(event, channel,"File removed due to contain explicit content")
+            post_message(event, channel, "File removed due to contain explicit content")
             
         return {"payload": "Done"}
         
@@ -68,11 +95,11 @@ def main(event):
     event_subtype = event_details.get('subtype')
     
     # skip reply messages sent from bot
-    if event_subtype=='bot_message':
+    if event_subtype == 'bot_message':
         return {"payload": "Done"}
     
     if message_text:
-        print('Text Message:'+message_text)
+        print('Text Message:' + message_text)
         natural_language_understanding = NaturalLanguageUnderstandingV1(
             username=event['NLU_USERNAME'],
             password=event['NLU_PASSWORD'],
@@ -115,6 +142,7 @@ def verify_token(event):
         return False
     return True
 
+
 def post_message(event,channel, text):
     url = event['SLACK_MESSAGE_POST_URL']
     data = urllib.parse.urlencode(
@@ -130,12 +158,13 @@ def post_message(event,channel, text):
     response = urllib.request.urlopen(request)
     print('Slack response: '+json.dumps(response.read().decode('utf-8'), indent=2))
 
+    
 def contain_image(event):
     event_details = event['event']
     file_subtype = event_details.get('subtype')
 
     if file_subtype != 'file_share':
-        print('Not a file event- ignoring event...')
+        print('Not a file event, ignoring event...')
         return False
 
     file_details = event_details['files']
@@ -143,22 +172,22 @@ def contain_image(event):
     file_size = file_details[0]['size']
     
     if mime_type not in SUPPORTED_IMAGES:
-        print('File is not an image- ignoring event...')
+        print('File is not an image, ignoring event...')
         return False
 
     return True
 
 
-def download_image(event,url):
-    request = urllib.request.Request(url, headers={'Authorization': 'Bearer %s' % event['SLACK_ACCESS_TOKEN']})
-    return urllib.request.urlopen(request).read()
+def download_image_requests(event,url):
+    response = requests.get(url,stream=True, headers={'Authorization': 'Bearer %s' % event['SLACK_ACCESS_TOKEN']})
+    return response
 
 
 def delete_image(event,file_id):
     url = event['SLACK_MESSAGE_DELETE_URL']
     data = urllib.parse.urlencode(
         (
-            ("token", ACCESS_TOKEN),
+            ("token", event['SLACK_ACCESS_TOKEN']),
             ("file", file_id)
         )
     )
